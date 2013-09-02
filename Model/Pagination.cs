ea8aec84e01320dd10e1f9493d28f3b0cf04e877
aupshaw <contact_au@ymail.com>
2013-09-01 23:43:08 -0500

    Additional mappings from pagination JSON

--- Model/Pagination.cs
+++ Model/Pagination.cs
@@ -7,5 +7,11 @@
         
         [JsonMapping("next_max_id", JsonMapping.MappingType.Primitive)]
         public string NextMaxId { get; set; }
+
+        [JsonMapping("next_min_id", JsonMapping.MappingType.Primitive)]
+        public string NextMinId { get; set; }
+
+        [JsonMapping("min_tag_id", JsonMapping.MappingType.Primitive)]
+        public string MinTagId { get; set; }
     }
 }
